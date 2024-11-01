a8931e68df5ccc1c44d20d1c6a1207c095d68d92
Callyel Alves <callyelalves@hotmail.com>
2024-11-01 15:06:45 -0300

    add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDtsdFr1ixFvAsEinQ9asziSlEqzAYjUKs",
+    authDomain: "teste-ionic-dd5c4.firebaseapp.com",
+    projectId: "teste-ionic-dd5c4",
+    storageBucket: "teste-ionic-dd5c4.appspot.com",
+    messagingSenderId: "1041013042530",
+    appId: "1:1041013042530:web:6ebe343d6e7aa196f3d03b",
+    measurementId: "G-WYCDJEPXRB"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
